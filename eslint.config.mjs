6036ec3889fc947e4dea0cda2a2d303d1d29182a
william <william.frazao91@gmail.com>
2025-02-04 10:21:14 -0300

    editor config e eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      "react-hooks/rules-of-hooks": "error",  // Garante o uso correto de hooks
+      "react-hooks/exhaustive-deps": "warn",  // Garante que as dependências estejam corretas
+      "react/prop-types": "off",              // Desativa a exigência de PropTypes
+      "react/react-in-jsx-scope": "off",      // Não exige 'import React' no Next.js
+      "@typescript-eslint/explicit-module-boundary-types": "off", // Permite funções sem tipagem explícita
+    },
+  },
 ];
 
 export default eslintConfig;
