dce43a9410e9b28b8cb9e68181681b7c40019e9d
william <william.frazao91@gmail.com>
2025-02-04 19:51:23 -0300

    configurando o prettier com eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,26 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
 
   {
     rules: {
-      "react-hooks/rules-of-hooks": "error",  // Garante o uso correto de hooks
-      "react-hooks/exhaustive-deps": "warn",  // Garante que as dependências estejam corretas
-      "react/prop-types": "off",              // Desativa a exigência de PropTypes
-      "react/react-in-jsx-scope": "off",      // Não exige 'import React' no Next.js
-      "@typescript-eslint/explicit-module-boundary-types": "off", // Permite funções sem tipagem explícita
-    },
-  },
-];
+      'react-hooks/rules-of-hooks': 'error', // Garante o uso correto de hooks
+      'react-hooks/exhaustive-deps': 'warn', // Garante que as dependências estejam corretas
+      'react/prop-types': 'off', // Desativa a exigência de PropTypes
+      'react/react-in-jsx-scope': 'off', // Não exige 'import React' no Next.js
+      '@typescript-eslint/explicit-module-boundary-types': 'off' // Permite funções sem tipagem explícita
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
